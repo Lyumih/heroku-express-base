acf324c4e51566685cd91b420ea90836f116ff86
Mikhail Ivanov <55839915+midevelop@users.noreply.github.com>
2019-11-13 10:19:11 +0300

    feat: test port enb

--- app.js
+++ app.js
@@ -2,11 +2,19 @@
 const express = require("express");
 // создаем объект приложения
 const app = express();
+// app.set('port', process.env.PORT || 3000);
+
 // определяем обработчик для маршрута "/"
 app.get("/", function (request, response) {
 
   // отправляем ответ
-  response.send("<h2>Привет Express!</h2>");
+  response.send("<h2>Привет Express!</h2>" + process.env.PORT);
 });
+// server.js
+console.log(process.env)
+const port = process.env.PORT;
+console.log(`Your PORT  is ${port}`);
+const NODE_ENV = process.env.NODE_ENV;
+console.log(`Your NODE_ENV is ${NODE_ENV}`);
 // начинаем прослушивать подключения на 3000 порту
-app.listen(3000);
+app.listen(process.env.PORT || 3001);
